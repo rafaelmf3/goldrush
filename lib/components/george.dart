1f8590e66b886e6b00080a3621a20a7676fe33ed
Rafael Marinho <rafael_mf3@hotmail.com>
2023-01-04 20:56:15 -0300

    add simple hud and joystick for control the character movement

--- lib/components/george.dart
+++ lib/components/george.dart
@@ -6,24 +6,42 @@ import 'package:goldrush/components/skeleton.dart';
 import 'package:goldrush/components/zombie.dart';
 import 'package:goldrush/components/character.dart';
 
+import 'hud/hud.dart';
+
 class George extends Character {
-  George({required Vector2 position, required Vector2 size, required double speed}) : super(position: position, size: size, speed: speed);
+  George(
+      {required this.hud,
+      required Vector2 position,
+      required Vector2 size,
+      required double speed})
+      : super(position: position, size: size, speed: speed);
+
+  final HudComponent hud;
+  late double walkingSpeed, runningSpeed;
 
   @override
   Future<void> onLoad() async {
     super.onLoad();
 
+    walkingSpeed = speed;
+    runningSpeed = speed * 2;
+
     var spriteImages = await Flame.images.load('george.png');
-    final spriteSheet = SpriteSheet(image: spriteImages, srcSize: Vector2(width, height));
+    final spriteSheet =
+        SpriteSheet(image: spriteImages, srcSize: Vector2(width, height));
 
-    downAnimation = spriteSheet.createAnimationByColumn(column: 0, stepTime: 0.2);
-    leftAnimation = spriteSheet.createAnimationByColumn(column: 1, stepTime: 0.2);
+    downAnimation =
+        spriteSheet.createAnimationByColumn(column: 0, stepTime: 0.2);
+    leftAnimation =
+        spriteSheet.createAnimationByColumn(column: 1, stepTime: 0.2);
     upAnimation = spriteSheet.createAnimationByColumn(column: 2, stepTime: 0.2);
-    rightAnimation = spriteSheet.createAnimationByColumn(column: 3, stepTime: 0.2);
-
-    changeDirection();
+    rightAnimation =
+        spriteSheet.createAnimationByColumn(column: 3, stepTime: 0.2);
 
     addHitbox(HitboxRectangle());
+
+    animation = downAnimation;
+    playing = false;
   }
 
   @override
@@ -32,6 +50,53 @@ class George extends Character {
 
     if (other is Zombie || other is Skeleton) {
       other.removeFromParent();
+      hud.scoreText.setScore(10);
     }
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    speed = hud.runButton.buttonPressed ? runningSpeed : walkingSpeed;
+
+    if (!hud.joystick.delta.isZero()) {
+      position.add(hud.joystick.relativeDelta * speed * dt);
+      playing = true;
+
+      switch (hud.joystick.direction) {
+        case JoystickDirection.up:
+        case JoystickDirection.upRight:
+        case JoystickDirection.upLeft:
+          animation = upAnimation;
+          currentDirection = Character.up;
+          break;
+        case JoystickDirection.down:
+        case JoystickDirection.downRight:
+        case JoystickDirection.downLeft:
+          animation = downAnimation;
+          currentDirection = Character.down;
+          break;
+        case JoystickDirection.left:
+          animation = leftAnimation;
+          currentDirection = Character.left;
+          break;
+        case JoystickDirection.right:
+          animation = rightAnimation;
+          currentDirection = Character.right;
+          break;
+        case JoystickDirection.idle:
+          animation = null;
+          break;
+      }
+    } else {
+      if (playing) {
+        stopAnimations();
+      }
+    }
+  }
+
+  void stopAnimations() {
+    animation?.currentIndex = 0;
+    playing = false;
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flame/components.dart';
 import 'package:flame/flame.dart';
 import 'package:flutter/material.dart';
 import 'package:flame/game.dart';
+import 'package:goldrush/components/hud/hud.dart';
 import 'package:goldrush/components/zombie.dart';
 import 'package:goldrush/components/skeleton.dart';
 import 'components/background.dart';
@@ -15,25 +16,31 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Flame.device.fullScreen();
   await Flame.device.setPortrait();
-  
+
   // Run the app, passing the games widget here
-  runApp(
-    GameWidget(game: goldRush)
-  );
+  runApp(GameWidget(game: goldRush));
 }
 
-class GoldRush extends FlameGame with HasCollidables {
-
+class GoldRush extends FlameGame
+    with HasCollidables, HasDraggables, HasTappables {
   @override
   Future<void> onLoad() async {
-    super.onLoad();
-    
+    var hud = HudComponent();
     add(Background());
-    add (George(position: Vector2(200, 400), size: Vector2(48.0, 48.0), speed: 40.0));
-    add (Zombie(position: Vector2(100, 200), size: Vector2(32.0, 64.0), speed: 20.0));
-    add (Zombie(position: Vector2(300, 200), size: Vector2(32.0, 64.0), speed: 20.0));
-    add (Skeleton(position: Vector2(100, 600), size: Vector2(32.0, 64.0), speed: 60.0));
-    add (Skeleton(position: Vector2(300, 600), size: Vector2(32.0, 64.0), speed: 60.0));
+    add(George(
+        hud: hud,
+        position: Vector2(200, 400),
+        size: Vector2(48.0, 48.0),
+        speed: 40.0));
+    add(Zombie(
+        position: Vector2(100, 200), size: Vector2(32.0, 64.0), speed: 20.0));
+    add(Zombie(
+        position: Vector2(300, 200), size: Vector2(32.0, 64.0), speed: 20.0));
+    add(Skeleton(
+        position: Vector2(100, 600), size: Vector2(32.0, 64.0), speed: 60.0));
+    add(Skeleton(
+        position: Vector2(300, 600), size: Vector2(32.0, 64.0), speed: 60.0));
     add(ScreenCollidable());
+    add(hud);
   }
 }
